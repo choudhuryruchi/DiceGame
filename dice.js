cb7c922c9fbee61bdd34aff7dd04cd44376acae4
choudhuryruchi <86044825+choudhuryruchi@users.noreply.github.com>
2021-06-17 14:07:11 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,11 +1,11 @@
 var randomnumber1 = Math.floor(Math.random()*6)+1; //generates a ramdom number from 1-6
-var randomimage1 = "dice" +randomnumber1 +".png"; // string to select the image
+var randomimage1 = "dice" +randomnumber1 +".PNG"; // string to select the image
 var randomsource1 =  randomimage1 ; // source of image
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomsource1);
 
 var randomnumber2 = Math.floor(Math.random()*6)+1; //generates a ramdom number from 1-6
-var randomimage2 = "dice" +randomnumber2 +".png"; // string to select the image
+var randomimage2 = "dice" +randomnumber2 +".PNG"; // string to select the image
 var randomsource2 = randomimage2 ; // source of image
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomsource2);
